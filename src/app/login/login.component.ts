6683337adbefbbed4c09327798df9c1053cc721d
jenila <jenila.jeni@gmail.com>
2021-11-25 16:55:02 +0530

    changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup,FormControl,Validators} from '@angular/forms';
 import { Router} from '@angular/router';
-import { HttpClient, HttpHeaders, HttpParams, } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -9,11 +9,8 @@ import { HttpClient, HttpHeaders, HttpParams, } from '@angular/common/http';
 })
 export class LoginComponent implements OnInit {
 
-
-
-
   profile=new FormGroup({
-    email_in:new FormControl('',Validators.required),
+    un_in:new FormControl('',Validators.required),
     pass_in:new FormControl('',Validators.required)
   })
   postId: any;
@@ -26,16 +23,16 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
 
   }
-  user_email!: any;
+  user_name!: any;
   user_pass!: string;
 onSubmit(){
-  this.user_email=this.profile.value.email_in;
+  this.user_name=this.profile.value.un_in;
   this.user_pass=this.profile.value.pass_in;
- if((this.user_email=="admin")&&(this.user_pass=="admin")){
+ if((this.user_name=="admin")&&(this.user_pass=="admin")){
 
-  localStorage.setItem('userdetails',this.user_email);
+  localStorage.setItem('userdetails',this.user_name);
 
-  this._router.navigate(['/products'], {queryParams: {user: this.user_email}} )
+  this._router.navigate(['/products'], {queryParams: {user: this.user_name}} )
 
   this.http.post<any>('https://fakestoreapi.com/products', { title: 'Angular POST Request Example' }).subscribe({
     next: data => {
